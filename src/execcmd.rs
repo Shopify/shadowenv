2b3e9210b0d82677f447dab5105ea0c0a834dd36
Burke Libbey <burke@libbey.me>
2019-08-07 13:07:08 -0400

    better comment

--- src/execcmd.rs
+++ src/execcmd.rs
@@ -11,7 +11,7 @@ pub fn run(data: Option<&str>, argv: Vec<&str>) -> Result<(), Error> {
         None                 => (),
     }
 
-    // if it returns, it's guaranteed to be an error.
+    // exec only returns if it was unable to start the new process, and it's always an error.
     let err = exec::Command::new(&argv[0]).args(&argv[1..]).exec();
     Err(err.into())
 }
