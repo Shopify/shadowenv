0956d2fb506b178ae07a0a87fb499e82fc55b3fe
Burke Libbey <burke@libbey.me>
2019-08-22 16:37:23 -0400

    shitty direnv support

--- src/assimilate.rs
+++ src/assimilate.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::process::Command;
 use failure::Error;
 use regex::Regex;
 
@@ -25,7 +26,7 @@ pub fn run(mode: Option<Types>) -> Result<(), Error> {
 fn convert_type(typ: Types) -> Result<(), Error> {
     match &typ {
         Types::dotenv         => convert_dotenv(),
-        Types::direnv         => not_implemented(typ),
+        Types::direnv         => convert_direnv(),
         Types::nix_shell_hook => not_implemented(typ),
         Types::ruby_version   => not_implemented(typ),
         Types::nvmrc          => not_implemented(typ),
@@ -40,6 +41,31 @@ fn not_implemented(typ: Types) -> Result<(), Error> {
     return Err(format_err!("assimilation type not yet implemented: {:?}", typ))
 }
 
+fn convert_direnv() -> Result<(), Error> {
+    let output = Command::new("direnv")
+        .arg("export")
+        .arg("json")
+        .output().unwrap_or_else(|e| {
+            panic!("failed to execute process: {}", e)
+    });
+
+    if !output.status.success() {
+        println!("direnv failed");
+    }
+
+    let s = String::from_utf8_lossy(&output.stdout);
+    let json: std::collections::HashMap<String, String> =
+        serde_json::from_str(&s).expect("JSON was not well-formatted");
+
+    for (key, value) in json {
+        if key.starts_with("DIRENV_") {
+            continue;
+        }
+        println!("(env/set \"{}\" \"{}\")", key, value);
+    }
+
+    Ok(())
+}
 
 fn convert_dotenv() -> Result<(), Error> {
     let ignore = Regex::new("^\\s*($|#)").unwrap();
@@ -51,6 +77,9 @@ fn convert_dotenv() -> Result<(), Error> {
             continue;
         }
         let parts: Vec<&str> = line.splitn(2, "=").collect();
+        if parts.len() != 2 {
+            panic!("invalid line in dotenv: {:?}", line);
+        }
         let key = parts[0].trim();
         let mut value = parts[1].trim();
         if value.starts_with("'") && value.ends_with("'") {
